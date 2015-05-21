cf731d28a8e6853377581619c2874f96ab52c7eb
Oleg G.Kapranov <lugatex@yahoo.com>
2015-05-21 09:35:50 +0300

    updated reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
   def index
     @reservations = Reservation.all
-    render html: @reservations
+    # render html: @reservations
+    render html: '<b>html goes here<b/>'.html_safe
   end
 end
